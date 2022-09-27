5328a6f5b77e25b3ca8245baee29f98e212f6030
myb12 <mohammadyasinbappy@outlook.com>
2022-09-27 23:42:27 +0600

    Version two small cards are designed

--- components/versionTwoSection/versionTwoSection.js
+++ components/versionTwoSection/versionTwoSection.js
@@ -4,26 +4,45 @@ import banner1 from '../../public/images/v2-banner1.png';
 import banner2 from '../../public/images/v2-banner2.png';
 import banner3 from '../../public/images/v2-banner3.png';
 import CustomSlider from '../customlider/customSlider';
+import styles from '../../styles/VersionTwoSection.module.css';
 
 const VersionTwoSection = () => {
     return (
-        <div className="row g-3">
+        <div className="row">
             <div className="col-md-8">
                 <CustomSlider image={banner1} version2 />
             </div>
             <div className="col-md-4">
-                <div className="row g-3">
+                <div className="row g-4">
                     <div className="col-12">
-                        <Image
-                            src={banner2}
-                            alt="Picture of the logo"
-                        />
+                        <div className='position-relative'>
+                            <Image
+                                src={banner2}
+                                alt="Picture of the banner"
+                                className='rounded-1'
+                            />
+                            <div className={styles.smallImageBody}>
+                                <p>Maecenas non erat</p>
+                                <h5>Cell Phones</h5>
+                                <small>Weekend Discount</small>
+                                <h4>$479.00 <span>589.00</span></h4 >
+                            </div>
+                        </div>
                     </div>
                     <div className="col-12">
-                        <Image
-                            src={banner3}
-                            alt="Picture of the logo"
-                        />
+                        <div className='position-relative'>
+                            <Image
+                                src={banner3}
+                                alt="Picture of the banner"
+                                className='rounded-1'
+                            />
+                            <div className={styles.smallImageBody}>
+                                <p>Maecenas non erat</p>
+                                <h5>Prepaid Phones</h5>
+                                <small>Weekend Discount</small>
+                                <h4>$389.00 <span>689.00</span></h4 >
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
